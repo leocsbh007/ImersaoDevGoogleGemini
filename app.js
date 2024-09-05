9383ddb9263079467456fc6a03efa9fa2562a7e4
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-09-05 14:47:12 -0300

    Criação do projeto e alterações da estrutura do projeto e inclusão de novas funcionalidades

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ function pesquisar() {
     let resultados = "";
 
     // Itera sobre cada dado da lista de dados
-    for (let dado of dados) {
+    for (let post of posts) {
         // Cria um novo elemento HTML para cada resultado
         resultados += `
             <div class="item-resultado">
                 <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
+                    <a href="#" target="_blank">${post.titulo}</a>
                 </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
+                <p class="descricao-meta">${post.descricao}</p>
+                <p class="descricao-meta">${post.nivel}</p>
+                <p class="descricao-meta">${post.data}</p>
+                <a href=${post.git} target="_blank">Mais informações no Git Hub</a>
             </div>
         `;
     }
 
     // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
+
+    console.log("Olá Mundo!!!")
 }
--- dados.js
+++ dados.js
@@ -1,17 +1,58 @@
-let dados = [
-    {
-        titulo: "Rayssa Leal",
-        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
-    },
-    {
-        titulo: "Beatriz Souza",
-        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
-    },
-    {
-        titulo: "Rebeca Andrade",
-        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
-    }
-];
+let posts = [
+        {
+          titulo: "Arduino: Piscando um LED - Seu Primeiro Projeto",
+          descricao: "Aprenda a montar seu primeiro circuito com um Arduino e um LED. Um guia passo a passo para iniciantes.",
+          nivel: "Iniciante",
+          data: "2023-11-20",
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Introdução à Eletrônica: Componentes Básicos",
+          descricao: "Conheça os componentes eletrônicos mais comuns e suas funções: resistores, capacitores, transistores e muito mais.",
+          nivel: "Iniciante",
+          data: "2023-11-25"          ,
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Criando um Sensor de Temperatura com Arduino e DHT11",
+          descricao: "Aprenda a ler dados de um sensor de temperatura e umidade DHT11 usando um Arduino e a biblioteca adequada.",
+          nivel: "Intermediário",
+          data: "2023-12-02",
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Controlando um Motor DC com Arduino: Teoria e Prática",
+          descricao: "Explore os fundamentos do controle de motores DC e implemente um circuito simples com Arduino.",
+          nivel: "Intermediário",
+          data: "2023-12-09",
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Construindo um Sistema de Irrigação Automática com Raspberry Pi",
+          descricao: "Crie um sistema de irrigação inteligente usando um Raspberry Pi, sensores de umidade e relés.",
+          nivel: "Avançado",
+          data: "2023-12-16",
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Redes LoRaWAN: Uma Introdução à IoT de Longa Distância",
+          descricao: "Descubra a tecnologia LoRaWAN e como construir seus próprios dispositivos IoT de baixo consumo de energia.",
+          nivel: "Avançado",
+          data: "2023-12-23",
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Projetos de Natal com Arduino: Ideias Criativas e Divertidas",
+          descricao: "Inspire-se em projetos natalinos para Arduino e crie decorações interativas e personalizadas.",
+          nivel: "Intermediário",
+          data: "2023-12-25",
+          git: "https://github.com/leocsbh007"
+        },
+        {
+          titulo: "Desafios e Soluções na Impressão 3D para Projetos de Eletrônica",
+          descricao: "Explore as possibilidades da impressão 3D para criar gabinetes, suportes e outros componentes personalizados para seus projetos.",
+          nivel: "Intermediário",
+          data: "2024-01-05",
+          git: "https://github.com/leocsbh007"
+        }
+      ];
